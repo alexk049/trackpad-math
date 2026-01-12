0f2b0af1c6b452c85c1ecbc1c6a1bece0b79562c
AlexK049 <akadlofa@gmail.com>
2026-01-11 22:46:12 -0500

    feat: made sidecar spawn in prod only

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -11,18 +11,20 @@ pub fn run() {
         )?;
       }
 
-      // use tauri_plugin_shell::ShellExt;
-      // let sidecar = app.shell().sidecar("trackpad-chars-backend").unwrap();
-      // let (mut rx, mut _child) = sidecar.spawn().expect("Failed to spawn sidecar");
+      if !cfg!(debug_assertions) {
+        use tauri_plugin_shell::ShellExt;
+        let sidecar = app.shell().sidecar("trackpad-chars-backend").unwrap();
+        let (mut rx, mut _child) = sidecar.spawn().expect("Failed to spawn sidecar");
 
-      // tauri::async_runtime::spawn(async move {
-      //   while let Some(event) = rx.recv().await {
-      //      if let tauri_plugin_shell::process::CommandEvent::Stdout(line) = event {
-      //           let line_str = String::from_utf8_lossy(&line);
-      //           log::info!("Sidecar: {}", line_str);
-      //      }
-      //   }
-      // });
+        tauri::async_runtime::spawn(async move {
+          while let Some(event) = rx.recv().await {
+             if let tauri_plugin_shell::process::CommandEvent::Stdout(line) = event {
+                  let line_str = String::from_utf8_lossy(&line);
+                  log::info!("Sidecar: {}", line_str);
+             }
+          }
+        });
+      }
       
       Ok(())
     })
